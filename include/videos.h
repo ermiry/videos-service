1e2dda021e739120b46a6021e87e81bfe5495175
ermiry <erick.salas@ermiry.com>
2021-08-17 23:09:56 -0500

    Added base main videos methods

---
+++ include/videos.h
@@ -0,0 +1,22 @@
+#ifndef _VIDEOS_H_
+#define _VIDEOS_H_
+
+#include "runtime.h"
+
+struct _HttpResponse;
+
+extern RuntimeType RUNTIME;
+
+extern unsigned int PORT;
+
+extern unsigned int CERVER_RECEIVE_BUFFER_SIZE;
+extern unsigned int CERVER_TH_THREADS;
+extern unsigned int CERVER_CONNECTION_QUEUE;
+
+// inits videos main values
+extern unsigned int videos_init (void);
+
+// ends videos main values
+extern unsigned int videos_end (void);
+
+#endif
