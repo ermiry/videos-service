fe1cd1d1da628223cbb7bb810a01f89e1f3d597b
ermiry <erick.salas@ermiry.com>
2021-08-17 23:10:05 -0500

    Added base service HTTP configuration

---
+++ src/main.c
@@ -0,0 +1,140 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <time.h>
+#include <signal.h>
+
+#include <cerver/cerver.h>
+#include <cerver/version.h>
+
+#include <cerver/http/http.h>
+#include <cerver/http/route.h>
+
+#include <cerver/utils/log.h>
+#include <cerver/utils/utils.h>
+
+#include "videos.h"
+#include "version.h"
+
+#include "routes/service.h"
+
+static Cerver *videos_service = NULL;
+
+void end (int dummy) {
+	
+	if (videos_service) {
+		cerver_stats_print (videos_service, false, false);
+		cerver_log_msg ("\nHTTP Cerver stats:\n");
+		http_cerver_all_stats_print ((HttpCerver *) videos_service->cerver_data);
+		cerver_log_line_break ();
+		cerver_teardown (videos_service);
+	}
+
+	(void) videos_end ();
+
+	cerver_end ();
+
+	exit (0);
+
+}
+
+static void videos_set_videos_routes (HttpCerver *http_cerver) {
+
+	/* register top level route */
+	// GET /videos
+	HttpRoute *main_route = http_route_create (REQUEST_METHOD_GET, "videos", videos_handler);
+	http_cerver_route_register (http_cerver, main_route);
+
+	/* register videos children routes */
+	// GET videos/version
+	HttpRoute *version_route = http_route_create (REQUEST_METHOD_GET, "version", videos_version_handler);
+	http_route_child_add (main_route, version_route);
+
+}
+
+static void start (void) {
+
+	videos_service = cerver_create (
+		CERVER_TYPE_WEB,
+		"videos-service",
+		PORT,
+		PROTOCOL_TCP,
+		false,
+		CERVER_CONNECTION_QUEUE
+	);
+
+	if (videos_service) {
+		/*** cerver configuration ***/
+		cerver_set_alias (videos_service, "videos");
+
+		cerver_set_receive_buffer_size (videos_service, CERVER_RECEIVE_BUFFER_SIZE);
+		cerver_set_thpool_n_threads (videos_service, CERVER_TH_THREADS);
+		cerver_set_handler_type (videos_service, CERVER_HANDLER_TYPE_THREADS);
+
+		/*** web cerver configuration ***/
+		HttpCerver *http_cerver = (HttpCerver *) videos_service->cerver_data;
+
+		http_cerver_set_uploads_path (http_cerver, "/var/uploads");
+		http_cerver_set_uploads_delete_when_done (http_cerver, true);
+
+		videos_set_videos_routes (http_cerver);
+
+		// add a catch all route
+		http_cerver_set_catch_all_route (http_cerver, videos_catch_all_handler);
+
+		// admin configuration
+		http_cerver_enable_admin_routes (http_cerver, true);
+		http_cerver_register_admin_file_system (http_cerver, "/var/uploads");
+		http_cerver_register_admin_file_system (http_cerver, "/home/videos/uploads");
+
+		if (cerver_start (videos_service)) {
+			cerver_log_error (
+				"Failed to start %s!",
+				videos_service->info->name
+			);
+
+			cerver_delete (videos_service);
+		}
+	}
+
+	else {
+		cerver_log_error ("Failed to create cerver!");
+
+		cerver_delete (videos_service);
+	}
+
+}
+
+int main (int argc, char const **argv) {
+
+	srand (time (NULL));
+
+	// register to the quit signal
+	(void) signal (SIGINT, end);
+	(void) signal (SIGTERM, end);
+
+	// to prevent SIGPIPE when writting to socket
+	(void) signal (SIGPIPE, SIG_IGN);
+
+	cerver_init ();
+
+	cerver_version_print_full ();
+
+	videos_version_print_full ();
+
+	if (!videos_init ()) {
+		start ();
+	}
+
+	else {
+		cerver_log_error ("Failed to init videos!");
+	}
+
+	(void) videos_end ();
+
+	cerver_end ();
+
+	return 0;
+
+}
