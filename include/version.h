d35006b028436cab691474553453aa9bc99f81b9
ermiry <erick.salas@ermiry.com>
2021-09-06 07:10:42 -0500

    Updated version to 0.3.1

--- include/version.h
+++ include/version.h
@@ -1,10 +1,10 @@
 #ifndef _VIDEOS_VERSION_H_
 #define _VIDEOS_VERSION_H_
 
-#define VIDEOS_VERSION			"0.3"
-#define VIDEOS_VERSION_NAME		"Version 0.3"
-#define VIDEOS_VERSION_DATE		"30/08/2021"
-#define VIDEOS_VERSION_TIME		"17:14 CST"
+#define VIDEOS_VERSION			"0.3.1"
+#define VIDEOS_VERSION_NAME		"Version 0.3.1"
+#define VIDEOS_VERSION_DATE		"06/09/2021"
+#define VIDEOS_VERSION_TIME		"07:10 CST"
 #define VIDEOS_VERSION_AUTHOR	"Erick Salas"
 
 // print full videos version information
